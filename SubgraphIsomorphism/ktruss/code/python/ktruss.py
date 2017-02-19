6e7f5e02774d430665f058e8191ffeb87d095a31
vijaygadepally <vijayg@ll.mit.edu>
2017-02-19 17:43:29 -0500

    adding python ktruss

---
+++ SubgraphIsomorphism/ktruss/code/python/ktruss.py
@@ -0,0 +1,115 @@
+import numpy as np
+import scipy as sp
+from scipy.sparse import csr_matrix
+from scipy.sparse import csc_matrix
+from scipy.sparse import coo_matrix
+from scipy.sparse import lil_matrix
+import time
+#Use the pandas package if available
+#import pandas as pd
+
+import scipy.io as sio
+
+
+###################################################
+###################################################
+def StrArrayRead(filename):
+    
+    f=open(filename,'r')
+    edgelist = []
+    with open(filename, 'r') as f:
+        for line in f:
+            edgelist.append(list(map(float, line.split('\t'))))
+    f.close()
+    return np.asarray(edgelist)
+
+
+
+###################################################
+###################################################
+def spAny(nparray):
+    
+    val=np.empty((nparray.shape[0],1))
+    for x in range(nparray.shape[0]):
+        if nparray[x,:].nnz > 0:
+            val[x]=1
+        else:
+            val[x]=0
+    return val
+
+
+###################################################
+###################################################
+def set_zero_rows(sparray, rowNum):
+    for row in rowNum:
+        sparray.data[sparray.indptr[row]:sparray.indptr[row+1]]=0
+
+###################################################
+###################################################
+def set_diag_val(sparray,val):
+    r,c=sparray.shape
+    for row in xrange(r):
+        sparray[row,row]=val
+
+
+###################################################
+###################################################
+def StrArrayWrite(nparray, filename):
+    
+    f=open(filename,"wt",buffering=20*(1024**2))
+    f=open(filename,"w")
+    data = [str(float(row[0])) + '\t' + str(float(row[1])) + '\n' for row in nparray]
+    f.write(''.join(data))
+    f.close()
+
+#Use Pandas if you have it
+#pd.DataFrame(nparray).to_csv(filename, sep='\t', header=False, index=False)
+
+
+def ktruss (inc_mat_file,k):
+    
+    ii=StrArrayRead(inc_mat_file)
+    
+    startTime=time.clock()
+    
+    E=csr_matrix(( ii[:,2], (ii[:,0]-1, ii[:,1]-1)), shape=(max(ii[:,0]),max(ii[:,1])))
+    
+    readTime=time.clock()
+    
+    tmp=np.transpose(E)*E
+    sizeX,sizeY=np.shape(tmp)
+    
+    print "Time to Read Data:  " + str(readTime-startTime) + "s"
+    print "Computing k-truss"
+    tmp.setdiag(np.zeros(sizeX),k=0)
+    #set_diag_val(tmp,0)
+    tmp.eliminate_zeros()
+    R= E * tmp
+    
+    s=lil_matrix(((R==2).astype(float)).sum(axis=1))
+    xc= (s >=k-2).toarray()
+    
+    while sum(xc) != sum(spAny(E)):
+        x,y=np.where(xc==False)
+        set_zero_rows(E, x)
+        E.eliminate_zeros()
+        tmp=np.transpose(E)*E
+        #set_diag_val(tmp,0)
+        sizeX,sizeY=np.shape(tmp)
+        (tmp.tolil()).setdiag(np.zeros(sizeX),k=0)
+        tmp.eliminate_zeros()
+        s=csr_matrix(((R==2).astype(float)).sum(axis=1))
+        xc= (s >=k-2).astype(int)
+
+    ktrussTime=time.clock()
+    print "Time to Compute k=truss:  " + str(ktrussTime-startTime) + "s"
+    return E
+
+
+###################################################
+# Graph Challenge benchmark
+# Developer: Dr. Vijay Gadepally (vijayg@mit.edu)
+# MIT
+###################################################
+# (c) <2015> Vijay Gadepally
+###################################################
