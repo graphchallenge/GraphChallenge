a91519cb5218ebb07ab6c0526caf8cfb75dd25a5
Kao <ed21319@edkao-mac.llan.ll.mit.edu>
2017-03-15 14:19:45 -0400

    Turned on the parallel update option for the graph-tool partition call. Also added commented on the input parameters.

--- StochasticBlockModel/code/c++/run_graph_tool_partition_alg_main.py
+++ StochasticBlockModel/code/c++/run_graph_tool_partition_alg_main.py
@@ -1,4 +1,4 @@
-""" This Python script runs the graph-tool graph partition algorithm by Tiago Peixoto. Tested with the Graph-tool v.2.16 module."""
+""" This Python script runs the graph-tool graph partition algorithm by Tiago Peixoto."""
 
 from run_graph_tool_partition_alg_support import *
 import graph_tool.all as gt
@@ -9,7 +9,11 @@ out_neighbors, in_neighbors, N, E, true_partition = load_graph(input_filename, T
 input_graph = gt.Graph()
 input_graph.add_edge_list([(i,j) for i in range(len(out_neighbors)) for j in out_neighbors[i][:,0]])
 t0 = timeit.default_timer()
-graph_tool_partition = gt.minimize_blockmodel_dl(input_graph, mcmc_args={'parallel':False}, mcmc_equilibrate_args={'verbose':False, 'epsilon':1e-4}, mcmc_multilevel_args={'r':2}, verbose=True)
+# the parallel switch determines whether MCMC updates are run in parallel, epsilon is the convergence threshold for
+# the nodal updates (smaller value is stricter), and the verbose option prints updates on each step of the algorithm.
+# Please refer to the graph-tool documentation under graph-tool.inference for details on the input parameters
+graph_tool_partition = gt.minimize_blockmodel_dl(input_graph, mcmc_args={'parallel':True},
+                                                 mcmc_equilibrate_args={'verbose':False, 'epsilon':1e-4}, verbose=True)
 t1 = timeit.default_timer()
 print('\nGraph partition took {} seconds'.format(t1-t0))
 evaluate_partition(true_partition, graph_tool_partition.get_blocks())
