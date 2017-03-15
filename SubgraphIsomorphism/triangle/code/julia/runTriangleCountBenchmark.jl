5333e065640273aef0269c76a5b3b41a497fd7dd
Siddharth Samsi <ssamsi@login-1.local>
2017-03-14 21:01:32 -0400

    updated run script for julia implementation

--- SubgraphIsomorphism/triangle/code/julia/runTriangleCountBenchmark.jl
+++ SubgraphIsomorphism/triangle/code/julia/runTriangleCountBenchmark.jl
@@ -5,7 +5,7 @@ include("trianglev2.jl");
 adj_mtx_file = "../../../data/A_adj.tsv"
 inc_mtx_file = "../../../data/A_inc.tsv"
 
-T1 = @elapsed N1 = triangle(adj_mtx_file, inc_mtx_file)
+T1 = @elapsed N1 = triangle(adj_mtx_file)
 T2 = @elapsed N2 = trianglev2(adj_mtx_file)
 
 println("\nimplementation I  : time = ", T1, " triangle count = ", N1);
