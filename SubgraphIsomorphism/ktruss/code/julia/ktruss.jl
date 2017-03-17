a8038eb0afad299c9872088a3bfa2c5bec5cc25a
Siddharth Samsi <ssamsi@login-1.local>
2017-03-17 13:16:10 -0400

    faster version of ktruss in Julia

--- SubgraphIsomorphism/ktruss/code/julia/ktruss.jl
+++ SubgraphIsomorphism/ktruss/code/julia/ktruss.jl
@@ -1,33 +1,34 @@
+function calcx(E, m, n, k)
+    tmp = E.'*E;
+    R = E * ( tmp - spdiagm( diag(tmp) ) );
+    r,c,v = findnz(R);
+    id = v.==2;
+    A = sparse( r[id], c[id], 1, m, n);
+    s = sum(A, 2);
+    x = s .< (k-2);    
+    return(x, !x);
+end
 
-function ktruss(adj_mtx_file, k)
-    if ~isfile( adj_mtx_file )
+function ktruss(inc_mtx_file, k)
+    if ~isfile( inc_mtx_file )
         println("unable to open input file");
         return (-1);
     end
 
     # load input data       
-    t_read_inc=@elapsed ii = readdlm( adj_mtx_file, '\t', Int64);
+    t_read_inc=@elapsed ii = readdlm( inc_mtx_file, '\t', Int64);
     println("incidence matrix read time : ", t_read_inc);
 
     t_create_inc=@elapsed E = sparse( ii[:,1], ii[:,2], ii[:,3] );
     println("sparse adj. matrix creation time : ", t_create_inc);
 
     #
-    tmp = E.'*E;
-    R = E * ( tmp - spdiagm(diag(tmp)) );    
-    id = sparse( R .== 2 );
-    s = sum( id, 2);
-
-    x = s .< (k-2);
-    xc = s .>= (k-2); # full matrix unless we cast it to sparse
+    tic();
+    m,n = size(E);
+    x, xc = calcx(E, m, n, k);
     while sum(xc) != sum( any(E,2) )
         E[find(x), :] = 0
-        R = E * ( tmp - spdiagm(diag(tmp)) );
-        id = sparse( R .== 2 );
-        s = sum( id, 2);
-
-        x  = s .< (k-2); # full matrix
-        xc = s .>= (k-2); # full matrix unless we cast it to sparse
+        x, xc = calcx(E, m, n, k);
     end
     
     return E
