1353b46b329068b4ed316ca2ecf75ff314b468c7
Frank Wanye <wanyef@mail.gvsu.edu>
2019-06-21 15:11:06 -0400

    Skeleton for sample stack for iterative sampling

---
+++ StochasticBlockPartition/code/python/samplestack.py
@@ -0,0 +1,26 @@
+class SampleStack(object):
+    def __init__(self, args):
+        # Load graph
+        # Create stack of samples
+        # Use List as stack
+        self.stack = list()
+
+    def _sample(self):
+        # Iteratively perform sampling
+        return None
+
+    def _push(self):
+        # Add a subsample to the stack
+        return None
+    
+    def _pop(self):
+        # Propagate a subsample's results up the stack
+        return None
+
+    def unstack(self):
+        # Propagate results back through the stack
+        return None
+
+    def tail(self):
+        # Get innermost sample
+        return None
